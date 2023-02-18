0284e3c8f9fff646c70628cf9474f4d6d23d47d0
amod-kadam-cm <amod@apples-MacBook-Pro.local>
2023-02-18 11:07:32 +0530

    First Iteration

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+/*output "cm_private_key" {
+  description = " Cloud Manthan Private Key "
+  sensitive   = true
+  value       = tls_private_key.cm_ca_private_key.private_key_pem
+}
+*/
---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+  }
+}
+
+provider "tls" {
+  # Configuration options
+}
